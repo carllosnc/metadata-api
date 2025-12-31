8a472103b3cc47a026ed41d6390d946e8bad7799
Carlos Costa <carllos.nc@gmail.com>
2025-12-31 11:56:15 -0300

    feat: add YouTube metadata retrieval utility functions and types

---
+++ src/providers/youtube.ts
@@ -0,0 +1,28 @@
+export type YouTubeMetadata = {
+  title: string
+  url: string
+  author_name: string
+  author_url: string
+  type?: string
+  height?: number
+  width?: number
+  version?: string
+  provider_name?: string
+  provider_url?: string
+  thumbnail_height?: number
+  thumbnail_width?: number
+  thumbnail_url?: string
+  html?: string
+}
+
+export const isYouTubeUrl = (url: string): boolean => {
+  const youtubeRegex = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.be)(\/.*)?$/;
+  return youtubeRegex.test(url);
+};
+
+export async function getMetaDataFromYoutube(url: string): Promise<YouTubeMetadata> {
+  const response = await fetch(`https://youtube.com/oembed?url=${url}&format=json`)
+  const data: YouTubeMetadata = await response.json()
+  return data
+}
+
