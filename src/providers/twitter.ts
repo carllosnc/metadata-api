08ea07d15964ded5ba2a3158e4aadef4ea420a1b
Carlos Costa <carllos.nc@gmail.com>
2026-01-06 15:09:37 -0300

    Implement Twitter provider using fxtwitter API

---
+++ src/providers/twitter.ts
@@ -0,0 +1,56 @@
+export interface TwitterApiMetadata {
+  code?: number
+  message?: string
+  tweet?: {
+    url?: string
+    id?: string
+    text?: string
+    raw_text?: {
+      text?: string
+      facets?: {
+        type?: string
+        indices?: [number, number]
+        id?: string
+        display?: string
+        original?: string
+        replacement?: string
+      }[]
+    }
+    author?: {
+      id?: string
+      name?: string
+      screen_name?: string
+      avatar_url?: string
+      banner_url?: string
+      description?: string
+      location?: string
+      url?: string
+      followers?: number
+      following?: number
+      joined?: string
+      likes?: number
+      media_count?: number
+      protected?: boolean
+      website?: string | null
+      tweets?: number
+      avatar_color?: string | null
+    }
+  }
+}
+
+export function isTwitterUrl(url: string): boolean {
+  return /^https?:\/\/(?:twitter|x)\.com\/\w+\/status\/\d+/.test(url)
+}
+
+export async function getMetaDataFromTwitter(url: string): Promise<TwitterApiMetadata> {
+  const myUrl = new URL(url)
+  const pathName = myUrl.pathname
+  const response = await fetch(`https://api.fxtwitter.com${pathName}`, {
+    headers: {
+      'User-Agent': 'MyAwesomeBot/1.0 (+http://example.com/myawesomebot)',
+    },
+  })
+  const data: TwitterApiMetadata = await response.json()
+  return data
+}
+
